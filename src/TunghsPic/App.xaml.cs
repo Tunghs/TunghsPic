5cf375dae6d6bff83bdcd0187676a7156bf397a0
Tunghs <juk08843@gmail.com>
2023-11-04 23:37:53 +0900

    Feat: "Bootstrap 추가"

--- src/TunghsPic/App.xaml.cs
+++ src/TunghsPic/App.xaml.cs
@@ -10,6 +10,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
+using TunghsPic.Common.Helper;
+
 namespace TunghsPic
 {
     /// <summary>
@@ -17,19 +19,66 @@ namespace TunghsPic
     /// </summary>
     public partial class App : Application
     {
+        #region Fields
+        private const string _APPLICATION_NAME_ = "TunghsPic";
+        #endregion
+
         public App()
         {
-            var services = ConfigureServices();
-            Ioc.Default.ConfigureServices(services);
+            new Bootstrapper();
         }
 
-        private static IServiceProvider ConfigureServices()
+        protected override void OnStartup(StartupEventArgs e)
         {
-            var services = new ServiceCollection();
+            base.OnStartup(e);
 
-            // ViewModel 등록
-
-            return services.BuildServiceProvider();
+            if (this.CheckDuplicateProcess())
+            {
+                MessageBox.Show("이미 실행중 입니다.", "Warning");
+            }
         }
+
+        #region Methodes
+        ///// <summary>
+        ///// 프로세스 중복 체크
+        ///// 이미 실행 중이라면 해당 Window Activate 처리
+        ///// </summary>
+        ///// <returns></returns>
+        private bool CheckDuplicateProcess()
+        {
+            bool isDuplcate = false;
+
+            if (ProcessChecker.Do(_APPLICATION_NAME_))
+            {
+                isDuplcate = true;
+
+                string processName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+                int currentProcess = System.Diagnostics.Process.GetCurrentProcess().Id;
+                System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcessesByName(processName);
+                foreach (System.Diagnostics.Process process in processes)
+                {
+                    if (currentProcess == process.Id)
+                    {
+                        continue;
+                    }
+
+                    // find MainWindow Title
+                    IntPtr hwnd = ProcessChecker.FindWindow(null, _APPLICATION_NAME_);
+                    if (hwnd.ToInt32() > 0)
+                    {
+                        //Activate it
+                        ProcessChecker.SetForegroundWindow(hwnd);
+
+                        WindowShowStyle command = ProcessChecker.IsIconicNative(hwnd) ? WindowShowStyle.Restore : WindowShowStyle.Show;
+                        ProcessChecker.ShowWindow(hwnd, command);
+                    }
+                }
+
+                Current.Shutdown();
+            }
+
+            return isDuplcate;
+        }
+        #endregion
     }
 }
---
+++ src/TunghsPic/Bootstrapper.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TunghsPic
+{
+    public class Bootstrapper
+    {
+        public Bootstrapper()
+        {
+            var services = ConfigureServices();
+            Ioc.Default.ConfigureServices(services);
+        }
+
+        /// <summary>
+        /// Configures the services for the application.
+        /// </summary>
+        /// <returns></returns>
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            // ViewModel 등록
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
